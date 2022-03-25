6837da06c6ae41d58bc7fd275be20ae62acf99de
shimul <shimul@gmail.com>
2022-03-26 04:35:25 +0600

    updated site

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import "./Cart.css"
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart)
+
+
     return (
-        <div>
-            <h2>This is cart part</h2>
-            <p>Selected items : {cart.length}</p>
+        <div className='cart'>
+            <h2>Selected Phon</h2>
+            {
+                cart.map(phon => <p>Name:{phon.name}</p>)
+            }
+            <button className='btn-carts'>
+                <p>CHOOSE 1 FOR ME</p>
+            </button>
+            <button className='btn-carts'>
+                <p>CHOOSE AGAIN</p>
+            </button>
+
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2>My React Projects</h2>
+        <div className='header'>
+            <h1>Phon Collection iPhon Brand</h1>
+            <h2>Choose Any Product</h2>
         </div>
     );
 };
