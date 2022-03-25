8fa00fb71839ef5b0d55bca858f35c1fae0cf84f
shimul <shimul@gmail.com>
2022-03-25 23:01:54 +0600

    create component commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header/Header';
+
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Rreact Projects</h2>
+    <div >
+      <Header></Header>
+
+      <Shop></Shop>
     </div>
   );
 }
